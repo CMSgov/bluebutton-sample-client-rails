2bd4fe727d888cb0f8bdfbeea984dc135c20bd16
Dave Tisza <dtisza@fearless.tech>
2018-08-09 14:56:54 -0400

    Create and update controllers
    - Update the application with refresh and before_action
    
    - Create bbdemo with authorize, callback and first endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+  require 'bluebutton_oauth_service'
+
+  def refresh_token
+    @bluebutton_oauth_service.refresh_access_token
+  end
+
+  # Remember state parameter for Oauth2
+  def change_session_bb_state
+    session[:bb_state]  = SecureRandom.hex(16)
+  end
+
+  # auth/refresh. Place this in a before_action in any controller needing access to the Bluebutton API.
+  def bluebutton_oauth_service_check
+    change_session_bb_state
+
+    # Setup oauth2 client service
+    @bluebutton_oauth_service = BluebuttonOauthService.new(session[:bb_acc_token],session[:bb_state])
+
+
+    if session[:bb_acc_token]
+      @bluebutton_oauth_service.restore_access_token
+      if @bluebutton_oauth_service.expired?
+        refresh_token
+        # Save access token to session
+        session[:bb_acc_token] = @bb_application_user.bb_acc_token
+      end	
+    end
+  end
 end
---
+++ app/controllers/bbdemo_controller.rb
@@ -0,0 +1,30 @@
+class BbdemoController < ApplicationController
+  before_action :bluebutton_oauth_service_check
+
+  def bluebutton_authorize
+    # Change state param on every authorize
+    change_session_bb_state
+  end
+
+  # Handler for Bluebutton authorization redirect/callback
+  def bluebutton_callback
+    bluebutton_oauth_service = BluebuttonOauthService.new(session[:bb_acc_token],session[:bb_state])
+    bluebutton_oauth_service.set_access_token(params[:code])
+    bluebutton_oauth_service.refresh_access_token
+    redirect_to bluebutton_endpoints_path, :notice => "you have been successfully authorized!"
+    # Save access token to session.  
+    session[:bb_acc_token] = bluebutton_oauth_service.bb_acc_token
+  end
+
+  def bluebutton_endpoints
+    # Get and save patient id for page displays
+    @bb_acc_token_json = JSON.parse(session[:bb_acc_token])
+    session[:bb_patient_id] = @bb_acc_token_json['patient']
+  end
+
+  def bluebutton_endpoint_userinfo
+    bluebutton_oauth_service = BluebuttonOauthService.new(session[:bb_acc_token],session[:bb_state])
+    bluebutton_oauth_service.restore_access_token
+    @bb_response = bluebutton_oauth_service.bb_get_userinfo
+  end
+end
