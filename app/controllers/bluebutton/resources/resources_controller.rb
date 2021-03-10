aa76d1532d57038cf33105a40d4afb2c238feed9
Nathan Dister <nathan.dister@adhocteam.us>
2021-03-10 18:04:58 -0500

    Use BB2/BFD v2 resource URLs

--- app/controllers/bluebutton/resources/resources_controller.rb
+++ app/controllers/bluebutton/resources/resources_controller.rb
@@ -9,37 +9,37 @@ class Bluebutton::Resources::ResourcesController < ApplicationController
 
   def userinfo
     resource_title('User Info')
-    @bb_response = @bluebutton_oauth_service.get_resource('/v1/connect/userinfo')
+    @bb_response = @bluebutton_oauth_service.get_resource('/v2/connect/userinfo')
     render('result')
   end
 
   def patient
     resource_title('Patient Information')
-    @bb_response = @bluebutton_oauth_service.get_resource('/v1/fhir/Patient/')
+    @bb_response = @bluebutton_oauth_service.get_resource('/v2/fhir/Patient/')
     render('result')
   end
 
   def eob
     resource_title('Explanation Of Benefit')
-    @bb_response = @bluebutton_oauth_service.get_resource('/v1/fhir/ExplanationOfBenefit/')
+    @bb_response = @bluebutton_oauth_service.get_resource('/v2/fhir/ExplanationOfBenefit/')
     render('result')
   end
 
   def coverage
     resource_title('Coverage')
-    @bb_response = @bluebutton_oauth_service.get_resource('/v1/fhir/Coverage/')
+    @bb_response = @bluebutton_oauth_service.get_resource('/v2/fhir/Coverage/')
     render('result')
   end
 
   def oidc
     resource_title('OIDC Discovery')
-    @bb_response = @bluebutton_oauth_service.get_resource('/.well-known/openid-configuration')
+    @bb_response = @bluebutton_oauth_service.get_resource('/.well-known/openid-configuration-v2')
     render('result')
   end
 
   def fhirmeta
     resource_title('FHIR Metadata')
-    @bb_response = @bluebutton_oauth_service.get_resource('/v1/fhir/metadata')
+    @bb_response = @bluebutton_oauth_service.get_resource('/v2/fhir/metadata')
     render('result')
   end
 
